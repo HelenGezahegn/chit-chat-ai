55d24ad46d2ae597cc87c30aa17ad491bae3be23
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-30 22:51:24 -0600

    fix: configureStore + setupListeners import

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 import { Provider } from "react-redux";
-import { configureStore, setupListeners } from "@reduxjs/toolkit/query";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 import { api } from "@/state/api";
 
 export const store = configureStore({
   reducer: { [api.reducerPath]: api.reducer },
-  middleware: (getDefault) => getDefault().cooncat(api.middleware)
+  middleware: (getDefault) => getDefault().concat(api.middleware)
 });
 setupListeners(store.dispatch);
 
