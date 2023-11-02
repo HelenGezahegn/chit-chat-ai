21635d5ad18f9ff82247ad05a6d52d61d5002e03
Helen Gezahegn <gezahegn@ualberta.ca>
2023-11-02 14:16:08 -0600

    fix: large chunks issue from node_modules when building vite app

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,5 +7,21 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
+  },
+  // Fix large chunks issue due to node_modules
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        }
+      }
+    }
   }
 });
